17ed5a38c67529f008b74fbd465fe0fe44001fa7
JohnoAlm <almenoliver02@gmail.com>
2024-11-27 15:23:27 +0100

    Added functionality to get user events from API with pagination and in memory cache.

--- LoggingProvider/Controllers/ReportingController.cs
+++ LoggingProvider/Controllers/ReportingController.cs
@@ -1,18 +1,35 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using LoggingProvider.Services;
+using Microsoft.AspNetCore.Mvc;
 
 namespace LoggingProvider.Controllers;
 
 [Route("")]
 [ApiController]
-public class ReportingController : ControllerBase
+public class ReportingController(ReportingService reportingService) : ControllerBase
 {
-    [HttpGet("/userEvents")]
-    public async Task<IActionResult> GetUserEvents([FromQuery] string sortBy, [FromQuery] string orderBy)
+    private readonly ReportingService _reportingService = reportingService;
+
+    [HttpGet("/getUserEvents")]
+    public async Task<IActionResult> GetUserEvents([FromQuery] int pageNumber = 1, [FromQuery] int size = 50)
     {
-        return Ok();
+        try
+        {
+            if (pageNumber < 1) return BadRequest("Page number must be 1 or greater.");
+            if (size < 1) return BadRequest("Page size must be 1 or greater");
+
+            var userEvents = await _reportingService.GetUserEventsAsync(pageNumber, size);
+
+            if (userEvents == null || !userEvents.Any()) return NotFound("No user events found for the requested page.");
+
+            return Ok(userEvents);
+        }
+        catch (Exception ex) 
+        {
+            return BadRequest(ex.Message);  
+        }
     }
 
-    [HttpGet("/adminEvents")]
+    [HttpGet("/getAdminEvents")]
     public async Task<IActionResult> GetAdminEvents()
     {
         return Ok();
